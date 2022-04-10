73534ac60ba85fb2af5fa9e57fb3470d05cfab89
thiago <thiagoautran@gmail.com>
2022-04-10 00:21:24 -0300

    .

--- Autransoft.Fluent.HttpClient.Lib/Base/HttpClientIntegration.cs
+++ Autransoft.Fluent.HttpClient.Lib/Base/HttpClientIntegration.cs
@@ -9,16 +9,16 @@ namespace Autransoft.Fluent.HttpClient.Lib.Base
     {
         public readonly System.Net.Http.IHttpClientFactory _clientFactory;
         public readonly IAutranSoftFluentLogger<Integration> _logger;
-        public readonly string _httpClientName;
+        public string HttpClientName { get; private set; }
 
         public HttpClientIntegration(IAutranSoftFluentLogger<Integration> logger, System.Net.Http.IHttpClientFactory clientFactory, string httpClientName)
         {
-            _httpClientName = httpClientName;
+            HttpClientName = httpClientName;
             _clientFactory = clientFactory;
             _logger = logger;
         }
 
-        public virtual System.Net.Http.HttpClient CreateHttpClient() => _clientFactory.CreateClient(_httpClientName);
+        public virtual System.Net.Http.HttpClient CreateHttpClient() => _clientFactory.CreateClient(HttpClientName);
         public RequestFluent<Integration> CreateFluentHttpClient() => CreateHttpClient().Fluent(_logger);
     }
 }
